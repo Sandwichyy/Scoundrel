7f88806f692864144d2f85c742ffaf8761784c36
Marshade3153 <ashaikh1@binghamton.edu>
2026-03-07 15:03:34 -0500

    skibid

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import random
 
 cards = [] #fill w cards
+discarded = []
 health = 20
 
 #if weapon DNE -1 else dmg value
@@ -14,11 +15,15 @@ for i in range(4):
 def fillRoom():
      for i in range(4):
           index = random.randInt(0, 51)
-          while (cards[index].suit == "Hearts" OR cards[index].suit == "Hearts") and (cards[index].rank > 10):
+          while (cards[index].suit == "Hearts" or cards[index].suit == "Hearts") and (cards[index].rank > 10):
                index = random.randInt(0, 51)
 
           room[i] = cards[index]
           cards.remove(index)
+
+def discard(slot):
+     discarded.append(room[slot])
+     room[slot] = -1
 
 while health > 0:
      fillRoom()
